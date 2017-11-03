09af953fe8908717f69ceb654920c5aee3a63f4a
AdityaGupta1 <adityag@gmail.com>
2017-11-02 23:34:37 -0700

    added table lines to ascii table

--- ASCIITester.java
+++ ASCIITester.java
@@ -2,7 +2,7 @@
 // Aditya Gupta and Maxwell Jardetzky
 
 /**
-   Tests the ASCII class.
+   Tests the ASCII class by printing a table with 4 columns
 */
 public class ASCIITester {
    public static void main(String[] args) {
@@ -12,12 +12,19 @@ public class ASCIITester {
 }
 
 /**
-   Prints out an ASCII table, code golf style.
+   Prints out an ASCII table with borders and stuff
+   Doesn't use box-drawing characters because they don't render correctly (they show up as question marks)
 */
 class ASCII {
    public void printChart(int numColumns) {
-      for (int i = 32; i < 129; i++) {
-         System.out.printf("%5s" + ((i - numColumns + 1) % numColumns == 0 ? "\n" : "  "), i + " " + (char)i);      
+      String horizontalLine = "";
+      for (int i = 0; i <= 9 * numColumns; i++) {
+         horizontalLine += (i % 9 == 0 ? "+" : "-");
+      }
+   
+      System.out.println(horizontalLine);
+      for (int i = 32; i <= 123; i++) {
+         System.out.printf("| %5s  " + ((i + 1) % numColumns == 0 ? "|\n" + horizontalLine + "\n" : ""), i + " " + (char)i);      
       }
    }
 }
