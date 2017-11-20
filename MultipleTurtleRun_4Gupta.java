15d5a1e1bc81a908b403a1db6ca4e279be81da37
AdityaGupta1 <adityag@gmail.com>
2017-11-20 14:48:20 -0800

    multiple turtle run

---
+++ MultipleTurtleRun_4Gupta.java
@@ -0,0 +1,124 @@
+// Arrays-A5: Flexi Turtle Race
+// Aditya Gupta, period 4
+
+import acm.program.GraphicsProgram;
+import acm.graphics.GTurtle;
+import acm.graphics.GRect;
+import acm.graphics.GLabel;
+import java.awt.Color;
+import java.awt.Point;
+import java.awt.Font;
+import javax.swing.JOptionPane;
+
+/*
+  Races four turtles to a finish line by moving them random amounts at a time
+  Winner is the first turtle whose head crosses the finish line
+  Winner moves to the bottom and does a "victory dance"
+*/
+public class MultipleTurtleRun_4Gupta extends GraphicsProgram {
+   GTurtle[] turtles;
+   GRect finishLine = new GRect(975, 0, 25, 500);
+   GLabel finishLineLabel = new GLabel("FINISH", 1010, 20);
+   Font labelFont = new Font("Arial", Font.BOLD, 18);
+   
+   GTurtle winner;
+   boolean turtleWon = false;
+   
+   Point victoryPoint = new Point(500, 700);
+   
+   public void run() {
+      String input;
+      int numberOfTurtles = 0;
+      
+      boolean isValid = false;
+      while (!isValid) {
+         input = JOptionPane.showInputDialog("How many turtles (2 to 9)?");
+      
+         try {
+            numberOfTurtles = Integer.parseInt(input);
+         } catch (NumberFormatException e) {
+            continue;
+         }
+         
+         if (numberOfTurtles < 2 || numberOfTurtles > 9) {
+            continue;
+           
+         }
+         
+         
+         isValid = true;
+      }
+      
+      turtles = new GTurtle[numberOfTurtles];
+      
+      for (int i = 0; i < turtles.length; i++) {
+         turtles[i] = new GTurtle(100, 50 + (i * (400 / turtles.length)));
+      }
+   
+      this.setSize(1200, 900 + 100);
+      
+      finishLine.setColor(Color.BLACK);
+      finishLine.setFilled(true);
+      add(finishLine);
+      finishLineLabel.setFont(labelFont);
+      add(finishLineLabel);
+   
+      for (GTurtle turtle : turtles) {
+         add(turtle);
+      }
+      
+      while (!allTurtlesFinished()) {
+         for (GTurtle turtle : turtles) {
+            if (!isTurtleFinished(turtle)) {
+               turtle.forward(20 + (Math.random() * 80));
+            }
+            
+            if (isTurtleFinished(turtle)) {
+               if (!turtleWon) {
+                  winner = turtle;
+                  turtleWon = true;
+               }
+            }
+         }
+         
+         pause(500);
+      }
+      
+      Point winnerPoint = new Point((int) (winner.getX() + winner.getWidth() / 2), (int) (winner.getY() + winner.getHeight() / 2));
+      double yDifference = Math.abs(victoryPoint.y - winnerPoint.y);
+      double xDifference = Math.abs(victoryPoint.x - winnerPoint.x);
+      winner.setDirection(180 + Math.toDegrees(Math.atan(yDifference / xDifference)));
+      
+      for (int i = 0; i < (int) winnerPoint.distance(victoryPoint) / 20; i++) {
+         winner.forward(20);
+      }
+      
+      // victory dance
+      for (int i = 0; i < 100; i++) {
+         winner.setDirection(Math.random() * 360);
+         winner.forward(Math.random() * 2 * i);
+      }
+   }
+   
+   private boolean isTurtleFinished(GTurtle turtle) {
+      if (turtle.getX() < finishLine.getX()) {
+         return false;
+      }
+      
+      return true;
+   }
+   
+   private boolean allTurtlesFinished() {
+      for (GTurtle turtle: turtles) {
+         if (!isTurtleFinished(turtle)) {
+            return false;
+         }
+      }
+      
+      return true;
+   }
+
+   public static void main(String[] args) {
+      new MultipleTurtleRun_4Gupta().start(args);
+   }
+}
